3c9e3b542962327f80247efc68a0cbccb69d0fce
MrPaivas <tiago.paiva@outlook.com.br>
2023-06-03 19:47:07 -0300

    Estudos pytest em classes simples

---
+++ erros em classes simples/main.py
@@ -0,0 +1,16 @@
+# TESTE DE ERRO EM UMA CLASSE SIMPLES
+# QUER AGUA MEU FIH?
+# QUERO NÃO
+# MEU FIH QUER CACHAÇA?
+# MIM DEEE PAPAI
+
+class MeuFihQuer():
+    def __init__(self, coisa):
+        self.coisa = coisa
+
+    def mim_deh_papai(self):
+        if self.coisa == 'CACHAÇA':
+            return True
+        else:
+            return False
+
